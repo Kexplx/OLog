7446a324e7d357ed8c1e62e2f85b52b3979b1f7c
oscar <oscar.rosner@gmx.de>
2018-06-17 16:22:04 +0200

    Adds better documentation /var names

--- OLogger/OLogger.cs
+++ OLogger/OLogger.cs
@@ -1,23 +1,38 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 namespace OLog
 {
     /// <summary>
-    /// A Logger to save log messages to the specified file
+    /// Main logical functions of the logger
     /// </summary>
     public class OLogger
     {
         private readonly string _path;
+        private object _lock = new object();
 
         /// <summary>
-        /// Initializes the logger and appends to the file under <paramref name="path"/>.
+        /// Enabling this property will lead to the logging messages being written to the console aswell.
+        /// </summary>
+        public bool LogToConsoleEnabled
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializes the logger and appends to the file under <paramref name="fileNameInBinFolder"/>.
         /// If the files doesn't exist it will be created
         /// </summary>
-        /// <param name="path">The path of the created LogFile</param>
-        public OLogger(string path)
+        /// <param name="fileNameInBinFolder">The path of the created LogFile</param>
+        /// <param name="logToConsoleEnabled">Also write to the console, if enabled</param>
+        public OLogger(string fileNameInBinFolder, bool logToConsoleEnabled = false)
         {
-            _path = path;
+            LogToConsoleEnabled = logToConsoleEnabled;
+            _path = fileNameInBinFolder;
+
+            using (var stream = File.Open(fileNameInBinFolder, FileMode.OpenOrCreate)) { }
         }
 
         /// <summary>
@@ -26,17 +41,9 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation Info(string message, Type outgoingClass)
+        public LogInformation Info(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
-            {
-                Message = DateTime.Now.ToString() + " INFO " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.INFO
-            };
-
-            File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
-
-            return logInformation;
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.INFO);
         }
 
         /// <summary>
@@ -45,17 +52,9 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation Error(string message, Type outgoingClass)
+        public LogInformation Error(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
-            {
-                Message = DateTime.Now.ToString() + " ERROR " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.ERROR
-            };
-
-            File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
-
-            return logInformation;
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.ERROR);
         }
 
         /// <summary>
@@ -64,17 +63,9 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation Warning(string message, Type outgoingClass)
+        public LogInformation Warn(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
-            {
-                Message = DateTime.Now.ToString() + " WARNING " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.WARNING
-            };
-
-            File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
-
-            return logInformation;
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.WARNING);
         }
 
         /// <summary>
@@ -83,17 +74,9 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation Fatal(string message, Type outgoingClass)
+        public LogInformation Fatal(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
-            {
-                Message = DateTime.Now.ToString() + " FATAL " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.FATAL
-            };
-
-            File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
-
-            return logInformation;
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.FATAL);
         }
 
         /// <summary>
@@ -102,17 +85,9 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation FinalInfo(string message, Type outgoingClass)
+        public LogInformation FinalInfo(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
-            {
-                Message = DateTime.Now.ToString() + " FINALINFO " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.FINALINFO
-            };
-
-            File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
-
-            return logInformation;
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.FINALINFO);
         }
 
         /// <summary>
@@ -121,17 +96,109 @@ namespace OLog
         /// <param name="message">The error message to log</param>
         /// <param name="outgoingClass">The class from which the error accured</param>
         /// <returns></returns>
-        public LogInformation CancelInfo(string message, Type outgoingClass)
+        public LogInformation CancelInfo(string message, Type loggedFromThisClassType)
         {
-            var logInformation = new LogInformation
+            return LogTheMessage(message, loggedFromThisClassType, LogMessageType.CANCELINFO);
+        }
+
+        private LogInformation LogTheMessage(string message, Type loggedFromThisClassType, LogMessageType type)
+        {
+            Monitor.Enter(_lock);
+            LogInformation logInformation = null;
+
+            switch (type)
             {
-                Message = DateTime.Now.ToString() + " CANCELINFO " + outgoingClass.FullName + " - " + message,
-                Type = LogMessageType.CANCELINFO
-            };
+                case LogMessageType.WARNING:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " WARNING " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.WARNING
+                    };
+                    break;
+
+                case LogMessageType.ERROR:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " ERROR " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.ERROR
+                    };
+                    break;
+
+                case LogMessageType.INFO:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " INFO " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.INFO
+                    };
+                    break;
+
+                case LogMessageType.FATAL:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " FATAL " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.FATAL
+                    };
+                    break;
+
+                case LogMessageType.FINALINFO:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " FINALINFO " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.FINALINFO
+                    };
+                    break;
+
+                case LogMessageType.CANCELINFO:
+                    logInformation = new LogInformation
+                    {
+                        Message = DateTime.Now.ToString() + " CANCELINFO " + loggedFromThisClassType.FullName + " - " + message,
+                        Type = LogMessageType.CANCELINFO
+                    };
+                    break;
+            }
 
             File.AppendAllText(_path, logInformation.Message + Environment.NewLine);
+            WriteToConsole(logInformation);
 
+            Monitor.Exit(_lock);
             return logInformation;
+        }
+
+        /// <summary>
+        /// Outputs the message to the console, with the specified foreground
+        /// </summary>
+        /// <param name="message"></param>
+        public void WriteToConsole(LogInformation message)
+        {
+            if (!LogToConsoleEnabled)
+            {
+                return;
+            }
+
+            switch (message.Type)
+            {
+                case LogMessageType.INFO:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(message.Message);
+                    Console.ResetColor();
+                    break;
+
+                case LogMessageType.ERROR:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(message.Message);
+                    Console.ResetColor();
+                    break;
+
+                case LogMessageType.WARNING:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(message.Message);
+                    Console.ResetColor();
+                    break;
+
+                default:
+                    Console.WriteLine(message.Message);
+                    break;
+            }
         }
     }
 }
